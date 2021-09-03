0f68331aaeac813678a6a746ead1c5596253eec1
MD Zahirul Islam <82234171+zahirulislam-iv@users.noreply.github.com>
2021-09-03 21:41:53 +0600

    Add files via upload

---
+++ source_code_protect.js
@@ -0,0 +1,6 @@
+
+//These codes are taken from stack overflow.
+//These codes are taken from stack overflow.
+function disableselect(e){return!1}function reEnable(){return!0}document.addEventListener("contextmenu",function(e){e.preventDefault()}),document.onkeydown=function(e){return 123!=e.keyCode&&((!e.ctrlKey||!e.shiftKey||e.keyCode!="I".charCodeAt(0))&&((!e.ctrlKey||!e.shiftKey||e.keyCode!="J".charCodeAt(0))&&((!e.ctrlKey||e.keyCode!="U".charCodeAt(0))&&((!e.ctrlKey||!e.shiftKey||e.keyCode!="C".charCodeAt(0))&&void 0))))},document.onselectstart=new Function("return false"),window.sidebar&&(document.onmousedown=disableselect,document.onclick=reEnable),$("a").attr("draggable",!1),$("img").attr("draggable",!1);
+//These codes are taken from stack overflow.
+//These codes are taken from stack overflow.
